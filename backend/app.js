c354372e11f5e30ecb343874bd027e73f2e27f47
Bibek Jana <168276117+bibekdotdev@users.noreply.github.com>
2025-10-25 22:58:35 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const channelRouter = require("./routes/channelRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
 
 // -------------------- MIDDLEWARES --------------------
 app.use(cookieParser());
