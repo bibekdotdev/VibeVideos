86067f40c0cf4e9e84e0e9007c18426485e15283
Bibek Jana <168276117+bibekdotdev@users.noreply.github.com>
2026-02-04 11:14:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const path = require("path");
 
 const Auth = require("./routes/auth");
 const videoRoutes = require("./routes/videoRoutes");
@@ -10,13 +10,6 @@ const channelRouter = require("./routes/channelRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-import path from "path";
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-
 app.use(cookieParser());
 app.use(
   cors({
@@ -44,6 +37,11 @@ app.use("/api/video", videoRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
+});
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // -------------------- START SERVER --------------------
