1b3d63446fe55621332ec5ee490056b2260695b1
Bibek Jana <168276117+bibekdotdev@users.noreply.github.com>
2026-02-04 11:24:38 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const path = require("path");
+
 
 const Auth = require("./routes/auth");
 const videoRoutes = require("./routes/videoRoutes");
@@ -10,8 +10,6 @@ const channelRouter = require("./routes/channelRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// -------------------- MIDDLEWARE --------------------
 app.use(cookieParser());
 app.use(
   cors({
@@ -22,25 +20,27 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// -------------------- API ROUTES --------------------
+
+const MONGO_URI =
+  "mongodb+srv://bibekjana68_db_user:jTIQGt3lk6XIKUWy@cluster0.l3xjsqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected successfully!"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+
 app.use("/api/auth", Auth);
 app.use("/api/channel", channelRouter);
 app.use("/api/video", videoRoutes);
 
-// -------------------- SERVE REACT --------------------
-app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
 
-// -------------------- DATABASE --------------------
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected successfully!"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-// -------------------- SERVER --------------------
+// -------------------- START SERVER --------------------
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
